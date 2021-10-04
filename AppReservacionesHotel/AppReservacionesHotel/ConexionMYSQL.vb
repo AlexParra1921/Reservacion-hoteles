4dea945feb73a3fc5defaff5b2265db1dad37e39
Reall <kevinduraan173@gmail.com>
2021-10-04 11:35:49 -0500

    Se agrega un objeto Usuario para guardar el tipo de usuario que se ha intentado logear

--- AppReservacionesHotel/AppReservacionesHotel/ConexionMYSQL.vb
+++ AppReservacionesHotel/AppReservacionesHotel/ConexionMYSQL.vb
@@ -6,19 +6,41 @@ Public Class ConexionMYSQL
 
     'Direccion del servidor MySQL , Objecto de conexion
     Private host = "localhost"
-    Public MysqlConexion As New MySqlConnection
-
+    Public mySqlConex As New MySqlConnection
+    Dim query As MySqlCommand = New MySqlCommand
+    Public userAuthenticated As Usuario
 
 
     'Subproceso para realizar la conexion
-    Public Sub conectar(user As String, pass As String)
-        MysqlConexion = New MySqlConnection("Server=" + host + ";" + "user=" + user + ";" + "password=" + pass + ";")
+    Function conectar(user As String, pass As String, database As String) As Boolean
+        mySqlConex = New MySqlConnection("Server=" + host + ";" + "database=reservaciones_hotel;" + "user=" + user + ";" + "password=" + pass + ";")
         Try
-            MysqlConexion.Open()
-            MsgBox("Conexion exitosa")
-            MysqlConexion.Close()
-        Catch ex As Exception
-            MsgBox("Error en la conexion")
+            mySqlConex.Open()
+
+
+            'Realizamos un query sobre la base de datos Reservaciones_hotel para dectectar que usuario
+            ' se ha intentado logear
+            query.CommandText = "SELECT TIPO FROM LOGIN WHERE cuenta='" & user & "'"
+
+            'Determinmos en que conexion se realiza el query
+            query.Connection = mySqlConex
+
+            'Se realiza la consulta por medio del adpatador
+            Dim da As MySqlDataAdapter = New MySqlDataAdapter(query)
+
+            'Recuperamos los registros del query realizado
+            Dim dt As DataTable = New DataTable
+            da.Fill(dt)
+
+            Dim tipo = CInt(dt.Rows(0)("tipo"))
+            userAuthenticated = New Usuario(user, pass, tipo)
+            Return True
+        Catch ex As MySqlException
+            MsgBox("Error en la conexion:" + ex.Message)
+            Return False
+        Finally
+            mySqlConex.Close()
         End Try
-    End Sub
+
+    End Function
 End Class
