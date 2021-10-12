c42cb605113a3a9a72ce08ea0369b0625a1d16d1
Reall <kevinduraan173@gmail.com>
2021-10-12 15:48:01 -0500

    Convertir funcion conectar en un subprocesos

--- AppReservacionesHotel/AppReservacionesHotel/ConexionMYSQL.vb
+++ AppReservacionesHotel/AppReservacionesHotel/ConexionMYSQL.vb
@@ -12,12 +12,10 @@ Public Class ConexionMYSQL
 
 
     'Subproceso para realizar la conexion
-    Function conectar(user As String, pass As String, database As String) As Boolean
+    Sub conectar(user As String, pass As String, database As String)
         mySqlConex = New MySqlConnection("Server=" + host + ";" + "database=reservaciones_hotel;" + "user=" + user + ";" + "password=" + pass + ";")
         Try
             mySqlConex.Open()
-
-
             'Realizamos un query sobre la base de datos Reservaciones_hotel para dectectar que usuario
             ' se ha intentado logear
             query.CommandText = "SELECT TIPO FROM LOGIN WHERE cuenta='" & user & "'"
@@ -32,15 +30,19 @@ Public Class ConexionMYSQL
             Dim dt As DataTable = New DataTable
             da.Fill(dt)
 
+
             Dim tipo = CInt(dt.Rows(0)("tipo"))
             userAuthenticated = New Usuario(user, pass, tipo)
-            Return True
-        Catch ex As MySqlException
-            MsgBox("Error en la conexion:" + ex.Message)
-            Return False
+        Catch ex1 As InvalidOperationException
+            Throw ex1
+        Catch ex2 As MySqlException
+            Throw ex2
+        Catch ex3 As Exception
+            'Excepcion en caso de que no surga algo imprevisto no manejado y lo muestre en la pantalla
+            MsgBox(ex3.Message, MsgBoxStyle.Critical, "Excepcion")
         Finally
             mySqlConex.Close()
         End Try
 
-    End Function
+    End Sub
 End Class
