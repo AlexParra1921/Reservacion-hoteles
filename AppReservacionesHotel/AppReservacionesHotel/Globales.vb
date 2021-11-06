7fc9879d0dcc89c943e8ec942c5c55f735f2336e
KevinDur4n <kevinduraan173@gmail.com>
2021-11-06 12:48:23 -0600

    Creacion de clase Persona

---
+++ AppReservacionesHotel/AppReservacionesHotel/Globales.vb
@@ -0,0 +1,8 @@
+﻿Module Globales
+    Enum tipoUser As Short
+        Administrador = 1
+        Empleado = 2
+        Cliente = 3
+        Huespede = 4
+    End Enum
+End Module
---
+++ AppReservacionesHotel/AppReservacionesHotel/Login/Telefono.vb
@@ -0,0 +1,47 @@
+﻿Public Class Telefono
+
+    Private numero As String
+    Private lada As String
+    Private idDueño As Short
+
+    Public Sub New()
+        numero = ""
+        lada = ""
+        idDueño = 0
+    End Sub
+
+    Public Sub New(IdDueño As String, numero As String, lada As String, tipoDueño As tipoDueño)
+        Me.numero = numero
+        Me.lada = lada
+        Me.idDueño = IdDueño
+    End Sub
+
+    Public Property getSetNumero() As String
+        Get
+            Return Me.numero
+        End Get
+        Set(value As String)
+            Me.numero = value
+        End Set
+    End Property
+
+    Public Property getSetlada() As String
+        Get
+            Return lada
+        End Get
+        Set(value As String)
+            Me.lada = value
+        End Set
+    End Property
+
+    Public Property getSetIdDueño As String
+        Get
+            Return idDueño
+        End Get
+        Set(value As String)
+            Me.idDueño = idDueño
+        End Set
+    End Property
+
+
+End Class
