7b3fb3a2643e3278921edd4d477a08348305c9f5
KevinDur4n <kevinduraan173@gmail.com>
2021-11-07 21:14:22 -0600

    Creacion de clase Cliente

---
+++ AppReservacionesHotel/AppReservacionesHotel/Login/Cliente.vb
@@ -0,0 +1,39 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class Cliente
+    Inherits Persona
+
+    Private id_cliente As Integer
+    Private id_Login As Integer
+
+    Public Sub New()
+        id_cliente = 0
+        id_Login = 0
+    End Sub
+
+    Public Sub New(id_cliente As Integer, id_login As Integer, nombre As String, apellidoPaterno As String, apellidoMaterno As String, edad As Integer)
+        MyBase.New(nombre, apellidoPaterno, apellidoMaterno, edad)
+        Me.id_cliente = id_cliente
+        Me.id_Login = id_login
+    End Sub
+
+
+    Public Property getSetidCliente() As Integer
+        Get
+            Return id_cliente
+        End Get
+        Set(value As Integer)
+            Me.id_cliente = value
+        End Set
+    End Property
+
+    Public Property getSetIdLogin() As Integer
+        Get
+            Return id_Login
+        End Get
+        Set(value As Integer)
+            Me.id_Login = id_Login
+        End Set
+    End Property
+
+End Class
