2219beed6689ecaa44368d3ab9a4902cfdb29417
KevinDur4n <kevinduraan173@gmail.com>
2021-11-11 17:55:52 -0600

    No recordar contraseñas cuando no se realiza conexion

--- AppReservacionesHotel/AppReservacionesHotel/Formularios/formLogin.vb
+++ AppReservacionesHotel/AppReservacionesHotel/Formularios/formLogin.vb
@@ -2,46 +2,40 @@
 
 
     Private Sub Button_Conectar_Click(sender As Object, e As EventArgs) Handles Button_Conectar.Click
-        'Realizamos la conexion a la base de datos y verificamos que usuario es el que se autentificado
-        'Si es empleado abrira el Formulario de control, si es Administrador Abrira el Control para Administrador
-        Dim conex As New ConexionMYSQL()
-
         'Verificamos lo campos si estan vacios 
         If TextBox_Cuenta.Text.Count < 1 Then
             label_error_user.Text = My.Resources.Vacio_Usuario.ToString
             Return
         End If
+
         If TextBox_Contraseña.Text.Count < 1 Then
             label_error_user.Text = My.Resources.Vacio_Contraseña
             Return
         End If
 
-        'Guarda usuario y contraseña
-        If check_remember.Checked Then
-            My.Settings.usuario = TextBox_Cuenta.Text
-            My.Settings.contrasena = TextBox_Contraseña.Text
-
-        Else
-            My.Settings.usuario = ""
-            My.Settings.contrasena = ""
-
-        End If
 
 
+        'Realizamos la conexion a la base de datos y verificamos que usuario es el que se autentificado
+        'Si es empleado abrira el Formulario de control, si es Administrador Abrira el Control para Administrador
+        Dim conex As New Mysql()
 
+        'Realiza la conexion a la base de datos con un usuario
         Try
-            conex.conectar(TextBox_Cuenta.Text, TextBox_Contraseña.Text, "reservaciones_hotel")
-            If conex.userAuthenticated.getSetTipo = 0 Then
+            conex.conectar(TextBox_Cuenta.Text, TextBox_Contraseña.Text, database_name)
+            If userAuthenticated.getSetTipo = 0 Then
                 Me.Hide()
                 ControlEmpleado.Show()
-            ElseIf conex.userAuthenticated.getSetTipo = 1 Then
+            ElseIf userAuthenticated.getSetTipo = 1 Then
                 MsgBox("Usted es administrador", MsgBoxStyle.Information, "Conexion sastifactoria")
             End If
+
+
         Catch ex As Exception
             label_error_user.Text = My.Resources.No_Usuario_contraseña
+
+            'Si hay un error en los textbox no guardar valores
+            check_remember.Checked = False
         End Try
-
-
 
 
     End Sub
@@ -57,9 +51,10 @@
 
     Private Sub LoginForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         check_remember.Checked = My.Settings.check_usrpass
+
         If My.Settings.usuario <> "" Then
-            TextBox_Cuenta.Text = My.Settings.usuario
-        End If
+                TextBox_Cuenta.Text = My.Settings.usuario
+            End If
 
         If My.Settings.contrasena <> "" Then
             TextBox_Contraseña.Text = My.Settings.contrasena
@@ -80,9 +75,16 @@
         If answer = MsgBoxResult.No Then
             e.Cancel = True
         End If
+        'Guarda usuario y contraseña
+        If check_remember.Checked Then
+            My.Settings.usuario = TextBox_Cuenta.Text
+            My.Settings.contrasena = TextBox_Contraseña.Text
+            My.Settings.check_usrpass = True
+        Else
+            My.Settings.usuario = ""
+            My.Settings.contrasena = ""
+            My.Settings.check_usrpass = False
+        End If
     End Sub
 
-    Private Sub check_remember_CheckedChanged(sender As Object, e As EventArgs) Handles check_remember.CheckedChanged
-        My.Settings.check_usrpass = check_remember.Checked
-    End Sub
 End Class
