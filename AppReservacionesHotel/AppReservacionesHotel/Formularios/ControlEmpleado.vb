c4c697be077352e903cb3d2d0e5eb3fa2a755ab1
KevinDur4n <kevinduraan173@gmail.com>
2021-11-13 15:19:41 -0600

    Diseño Control Empleado Azul

--- AppReservacionesHotel/AppReservacionesHotel/Formularios/ControlEmpleado.vb
+++ AppReservacionesHotel/AppReservacionesHotel/Formularios/ControlEmpleado.vb
@@ -1,5 +1,5 @@
 ﻿Public Class ControlEmpleado
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles bt_habitaciones.Click
 
     End Sub
 
@@ -15,6 +15,18 @@
             Return
         End If
         LoginForm.Show()
+
+    End Sub
+
+    Private Sub Control_Click(sender As Object, e As EventArgs) Handles bt_reservaciones.Click
+
+    End Sub
+
+    Private Sub HabitacionToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HabitacionToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub CleinteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CleinteToolStripMenuItem.Click
 
     End Sub
 End Class
