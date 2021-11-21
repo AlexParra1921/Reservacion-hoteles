a3f1a7a212d2931adf511a70bee2ac5ad3ee955d
KevinDur4n <kevinduraan173@gmail.com>
2021-11-21 00:48:15 -0600

    Se agrega metodo para realizar la reserva de las habitaciones mediante transacciones

--- AppReservacionesHotel/AppReservacionesHotel/Accesos/Reservaciones.vb
+++ AppReservacionesHotel/AppReservacionesHotel/Accesos/Reservaciones.vb
@@ -1,10 +1,12 @@
-﻿Public Class Reservaciones
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Reservaciones
     Private id_reservaciones As Integer
     Private id_empleado As Integer
     Private id_cliente As Integer
-    Private fechaEntrada As DateTime
-    Private fechaSalida As DateTime
-    Private fechaReservacion As DateTime
+    Private fechaEntrada As String
+    Private fechaSalida As String
+    Private fechaReservacion As String
     Private costo As Decimal
     Private num_dias As Integer
     Private no_habitaciones As Integer
@@ -21,6 +23,9 @@
         Me.costo = costo
         Me.num_dias = num_dias
         Me.no_habitaciones = no_habitaciones
+    End Sub
+
+    Public Sub New()
     End Sub
 
     Public Property gs_id_reservacion As Integer
@@ -50,29 +55,29 @@
         End Set
     End Property
 
-    Public Property gs_fechaEntrada As Date
+    Public Property gs_fechaEntrada As String
         Get
             Return fechaEntrada
         End Get
-        Set(value As Date)
+        Set(value As String)
             fechaEntrada = value
         End Set
     End Property
 
-    Public Property gs_fechaSalida As Date
+    Public Property gs_fechaSalida As String
         Get
             Return fechaSalida
         End Get
-        Set(value As Date)
+        Set(value As String)
             fechaSalida = value
         End Set
     End Property
 
-    Public Property gs_FechaReservacion As Date
+    Public Property gs_FechaReservacion As String
         Get
             Return fechaReservacion
         End Get
-        Set(value As Date)
+        Set(value As String)
             fechaReservacion = value
         End Set
     End Property
@@ -106,15 +111,16 @@
 
     Private Function validarCampos() As Boolean
         If gs_id_reservacion = 0 Then
-        ElseIf gs_id_cliente = 0 Then
             Throw New ArgumentException("Id de reservacion invalido")
+        ElseIf gs_id_cliente = 0 Then
+            Throw New ArgumentException("Id de cliente Invalido")
         ElseIf gs_id_empleado = 0 Then
             Throw New ArgumentException("Id de empleado invalido")
         ElseIf gs_No_habitaciones = 0 Then
             Throw New ArgumentException("Numero de habitaciones invalidas")
         ElseIf gs_num_dias = 0 Then
             Throw New ArgumentException("Numero de dias Invalido")
-        ElseIf gs_FechaReservacion <> DateTime.Now Then
+        ElseIf gs_FechaReservacion = "" Then
             Throw New ArgumentException("Fecha de Reservacion Invalida")
         ElseIf gs_fechaEntrada = DateTime.Now Then
             Throw New ArgumentException("Fecha de entrada invalida")
@@ -204,4 +210,84 @@
         End Try
     End Sub
 
+    'Metodo para realizar la reservacion de las habitacion, se utilizara transacciones para agrupar todas 
+    'las consultas en una sola, en caso de que falle alguna se realizara un rollback
+    'Utilizamos una lista que contiene  el listado de id de habitaciones
+
+    Public Function reservar(ByVal listaId_habitaciones As List(Of Integer)) As Boolean
+        Dim command As MySqlCommand = cnx.CreateCommand()
+        Dim Transac As MySqlTransaction
+
+        'CREAMOS LA TRANSACCION
+        Transac = cnx.BeginTransaction()
+        command.Connection = cnx
+        command.Transaction = Transac
+
+        Try
+            'VALIDAMOS QUE LOS CAMPOS PARA LA RESERVACION ESTEN CORRECTOS
+            validarCampos()
+            If listaId_habitaciones.Count = 0 Then
+                Throw New ArgumentException("Habitaciones ingresadas '0'")
+            End If
+
+            Try
+                command.CommandText = String.Format("insert into reservacion values({0},{1},{2},'{3}','{4}','{5}',{6},{7},{8})", id_reservaciones, id_empleado, id_cliente,
+                                   fechaEntrada.ToString, fechaSalida.ToString, fechaReservacion.ToString, costo, num_dias, no_habitaciones)
+                command.ExecuteNonQuery()
+            Catch ex As Exception
+                Throw New Exception(ex.Message)
+            End Try
+
+            For Each id_habitacion As Integer In listaId_habitaciones
+                'REALIZA LA RESERVACION DE CADA HABITACION INSERTANDO EN LA TABLA UN REGISTRO QUE MARCA QUE HABITACION TIENE RESERVACION 
+                Try
+                    command.CommandText = String.Format("insert into habitacion_has_reservacion values({0},{1})", id_reservaciones, id_habitacion)
+                    command.ExecuteNonQuery()
+                Catch ex As Exception
+                    Throw New Exception(String.Format("Error al reservar habitacion {0} \n {1}", id_habitacion, ex.Message))
+                End Try
+                'MARCA LA HABITACION COMO OCUPADA
+                Try
+                    command.CommandText = String.Format("update habitacion set disponibilidad=0 where No_habitacion={0};", id_habitacion)
+                    command.ExecuteNonQuery()
+                Catch ex As Exception
+                    Throw New Exception(String.Format("Error al marcar habitacion Ocupada habitacion {0} \n {1}", id_habitacion, ex.Message))
+                End Try
+
+            Next
+
+            'SI TODAS LAS TRANSACCIONES SE REALIZARON SE REALIZA EL COMMIT
+            Transac.Commit()
+            reservar = True
+        Catch ex As Exception
+            Console.WriteLine("Error al realizar reservacion de las habitaciones \n Mensaje de error ----> " & ex.Message)
+            reservar = False
+            Try
+                Transac.Rollback()
+            Catch ex2 As Exception
+                MsgBox("Hubo un erro al realizar la reservacion de habitaciones, la reservacion esta incompleta, comunicace con su DBA!" & ex2.Message, MsgBoxStyle.Critical, "Error")
+            End Try
+
+        End Try
+    End Function
+
+
+
+
+
+
+    'METODO PARA BUSCAR EL ULTIMO ID DE RESERVACION
+    Public Sub asignarIdReservacion()
+        Dim strSql As String
+        Dim xCnx As New Mysql
+        Dim xDT As DataTable
+        strSql = String.Format("Select MAX(id_reservacion) as 'last id' from reservacion")
+
+        xDT = xCnx.objetoDataAdapter(strSql)
+        If xDT.Rows.Count = 1 Then
+            id_reservaciones = xDT.Rows(0)("last id") + 1
+        End If
+
+
+    End Sub
 End Class
